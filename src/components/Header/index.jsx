dda63c610f38a7eaca7fad5e6066dedf23b79d17
Eduardo Tavares <eduardotdev.21@gmail.com>
2024-01-02 23:33:31 -0300

    imagem do usuario

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,16 @@
-import { Container } from './styles'
+import { Container, Profile } from './styles'
 
 export function Header() {
-  return <Container></Container>
+  return (
+    <Container>
+      <Profile>
+        <img src="https://github.com/eduardotdev.png" alt="Foto do usuário" />
+
+        <div>
+          <span>Bem-vindo</span>
+          <strong>Eduardo Tavares</strong>
+        </div>
+      </Profile>
+    </Container>
+  )
 }
