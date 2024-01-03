c430595994cd6138c23da1da0732874a8e62fffa
Eduardo Tavares <eduardotdev.21@gmail.com>
2024-01-02 23:28:53 -0300

    componente header

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,15 +1,13 @@
 import { Container } from './styles'
 
+import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
 
 export function Details() {
   return (
     <Container>
-      <h1>Hello World!</h1>
-      <span>Eduardo Tavares</span>
+      <Header />
 
-      <Button title="Entrar" loading />
-      <Button title="Cadastrar" />
       <Button title="Voltar" />
     </Container>
   )
