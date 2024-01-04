3eb557be23fb65695c023110dde9c9ddc4836388
Eduardo Tavares <eduardotdev.21@gmail.com>
2024-01-03 23:14:01 -0300

    componente de notas

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,6 +4,8 @@ import { Brand, Container, Content, Menu, NewNote, Search } from './styles'
 import { Header } from '../../components/Header'
 import { ButtonText } from '../../components/ButtonText'
 import { Input } from '../../components/Input'
+import { Section } from '../../components/Section'
+import { Note } from '../../components/Note'
 
 export function Home() {
   return (
@@ -30,7 +32,19 @@ export function Home() {
         <Input placeholder="Pesquisar pelo título" />
       </Search>
 
-      <Content></Content>
+      <Content>
+        <Section title="Minhas notas">
+          <Note
+            data={{
+              title: 'React',
+              tags: [
+                { id: '1', name: 'react' },
+                { id: '2', name: 'rocketseat' },
+              ],
+            }}
+          />
+        </Section>
+      </Content>
 
       <NewNote>
         <FiPlus />
